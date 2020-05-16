7a81625036022bb6cd35167a9fbc0702cde94c8c
azhwani <azh.abderrahim@gmail.com>
2020-05-16 14:23:06 +0100

    updates CallNotificationSender

---
+++ notification-sender-api/src/main/java/com/sender/demo/CallNotificationSender.java
@@ -0,0 +1,14 @@
+package com.sender.demo;
+
+import com.sender.demo.services.NotificationSenderService;
+
+public class CallNotificationSender implements NotificationSenderService {
+
+    @Override
+    public void sendNotification(String contactInfo, SenderTemplate template) {
+        // TODO Auto-generated method stub
+
+        System.out.println("Phone Call Notification");
+    }
+
+}
